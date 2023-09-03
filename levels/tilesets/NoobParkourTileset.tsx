9d90453432716c26d1518358b6e35b75a3349b0b
Dmitry Mozgin <m0391n@gmail.com>
2023-09-04 01:48:49 +0300

    Add a tileset and character animations.

---
+++ levels/tilesets/NoobParkourTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="NoobParkourTileset" tilewidth="16" tileheight="16" tilecount="40" columns="10">
+ <image source="NoobParkourTileset.png" width="160" height="64"/>
+</tileset>
