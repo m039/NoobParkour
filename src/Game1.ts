dc03bc042125872beccf46deb76ee7a38b5b73c9
Dmitry Mozgin <m0391n@gmail.com>
2023-09-04 22:13:07 +0300

    Character stands on the ground

---
+++ src/Game1.ts
@@ -0,0 +1,69 @@
+import * as Phaser from 'phaser';
+import Player from "./Player";
+
+export default class Level1 extends Phaser.Scene
+{
+    constructor()
+    {
+        super();
+    }
+
+    preload()
+    {
+        this.load.tilemapTiledJSON("map", "assets/levels/maps/Level1.json");
+        this.load.image("tiles", "assets/levels/tilesets/NoobParkourTileset.png");
+        this.load.aseprite("noob", "assets/animations/NoobMain.png", "assets/animations/NoobMain.json");
+    }
+
+    create()
+    {
+        const map = this.make.tilemap({ key: "map"});
+        const tileset = map.addTilesetImage("NoobParkourTileset", "tiles");
+        const groundLayer = map.createLayer("Ground", tileset);
+        const gateLayer = map.createLayer("Back", tileset);
+        const tags = this.anims.createFromAseprite("noob");
+        const player = new Player(this);
+
+        groundLayer.setCollisionByExclusion([-1]);
+
+        this.physics.add.collider(player.container, groundLayer);
+
+        this.placeCharacterAtStart(player, map);
+        this.centerCameraAtCharacter(player);
+        this.cameras.main.setZoom(4, 4);
+    }
+
+    private placeCharacterAtStart(
+        player: Player, 
+        map: Phaser.Tilemaps.Tilemap) {
+        const startGate = map.findObject("Objects", o => o.name === "Start");
+
+        player.setPosition(startGate.x + startGate.width / 2, startGate.y + startGate.height / 2);
+    }
+
+    private centerCameraAtCharacter(player: Player) {
+        this.cameras.main.centerOn(player.getPosition().x, player.getPosition().y);
+    }
+}
+
+const config : Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.ENVELOP,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080,
+    },
+    backgroundColor: '#125555',
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: { y: 600}
+        }
+    },
+    scene: Level1
+};
+
+const game = new Phaser.Game(config);
--- src/Player.ts
+++ src/Player.ts
@@ -1,10 +1,15 @@
 import * as Phaser from 'phaser';
 
 export default class Player {
-    sprite : Phaser.GameObjects.Sprite;
-
+    container : Phaser.GameObjects.Container;
+    private sprite : Phaser.GameObjects.Sprite;
+    
     constructor(scene: Phaser.Scene) {
-        this.sprite = scene.add.sprite(400, 400, "noob").play({key:"Idle", repeat: -1});
+        this.container = scene.add.container(400, 400);
+        this.sprite = scene.add.sprite(-1, -2, "noob").play({key:"Idle", repeat: -1});
+        this.container.add(this.sprite);
+        this.container.setSize(13, 16);
+        scene.physics.add.existing(this.container);
     }
 
     public setPosition(x?:number, y?:number) {
@@ -12,14 +17,14 @@ export default class Player {
             x = 0;
         }
 
-        if (y == undefined) {
+        if (y === undefined) {
             y = 0;
         }
 
-        this.sprite.setPosition(x, y);
+        this.container.setPosition(x, y);
     }
 
     public getPosition() : Phaser.Math.Vector2 {
-        return new Phaser.Math.Vector2(this.sprite.x, this.sprite.y);
+        return new Phaser.Math.Vector2(this.container.x, this.container.y);
     }
 }
